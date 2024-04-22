cd0b1c7958ff9d43cf605e8ae0cbb8eac628c0bd
include-yy <969041171@qq.com>
2024-04-22 16:23:35 +0900

    first commit

---
+++ yynt.el
@@ -0,0 +1,35 @@
+;;; yynt.el --- simple org export manager -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 include-yy <yy@egh0bww1.com>
+
+;; Author: include-yy <yy@egh0bww1.com>
+;; Maintainer: include-yy <yy@egh0bww1.com>
+;; Created: 22 Apr 2024
+
+;; Package-Version: 0.1
+;; Package-Requires: ((emacs "29.2"))
+;; Keywords: manager
+;; URL: https://github.com/include-yy/yynt
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+
+;;; yynt.el ends here
